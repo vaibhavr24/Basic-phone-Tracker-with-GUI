76eed4e571c616d2362a4a14da9b87428fef14a4
Vaibhav Rajput <73458444+vaibhavr24@users.noreply.github.com>
2021-10-29 14:30:47 +0530

    Add files via upload

---
+++ basic_phn_tracker.py
@@ -0,0 +1,42 @@
+from tkinter import *
+from typing import Text
+import phonenumbers as ph
+from phonenumbers import geocoder, carrier
+
+oni = Tk()
+
+oni.title('LOCATION TRACKER')
+oni.geometry('400x300')
+label = Label(oni, text='Advance Project', height=2,font=("Felix Titling", 20)).pack()
+
+ent = Entry(oni, width=30, borderwidth=3)
+ent.pack()
+ent.insert(0, "+(Country Code)(Number)")
+ent.configure(state=DISABLED)
+
+# on click func
+
+
+def on_click(event):
+    ent.configure(state=NORMAL)
+    ent.delete(0, END)
+
+
+ent.bind("<Button-1>", on_click)
+
+
+# Main Func
+
+#ch_num = ph.parse(ent.get(), "CH")
+
+def called():
+    ch_num = ph.parse(ent.get(), "CH")
+    ser_num = ph.parse(ent.get(), "RO")
+    time_zone = ph.parse(ent.get())
+    myLabel = Label(oni, text=geocoder.description_for_number(ch_num, "en")).pack()
+    myLabel2 = Label(oni, text=carrier.name_for_number(ser_num, "en")).pack()
+
+
+bt = Button(oni, text="Find", font=("Felix Titling", 10),bg='white', fg='black', command=called).pack()
+
+oni.mainloop()
